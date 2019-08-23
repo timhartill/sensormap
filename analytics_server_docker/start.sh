2846d0bf7d800c60d4b88746d4002fb03ca57cca
timhartill <timhartill@gmail.com>
2019-08-23 16:28:07 +1200

    Update start.sh

--- analytics_server_docker/start.sh
+++ analytics_server_docker/start.sh
@@ -2,8 +2,6 @@
 
 export IP_ADDRESS=$(ip addr show wlp4s0 | awk '/inet / {print $2}' | cut -d/ -f1)
 
-export GOOGLE_MAP_API_KEY=AIzaSyCHKNpfCWryDzxbHMN_jKX05_PF-O1BTlg
-
 sudo -E docker-compose up -d
 
 
